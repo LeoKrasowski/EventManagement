e3ac4d720cfee4b7fcbd6fb952925156c7ac4874
LeoKrasowski <leokrasowski@gmail.com>
2024-05-12 22:00:36 +0200

    Initial commit

---
+++ deployment/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "C:/Users/A200119422/Mendix/EventManagement/themesource/administration/native/main";
+import * as style1 from "C:/Users/A200119422/Mendix/EventManagement/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/A200119422/Mendix/EventManagement/themesource/webactions/native/main";
+import * as style3 from "C:/Users/A200119422/Mendix/EventManagement/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/A200119422/Mendix/EventManagement/themesource/feedbackmodule/native/main";
+import * as style5 from "C:/Users/A200119422/Mendix/EventManagement/themesource/eventmanagement/native/main";
+import * as style6 from "C:/Users/A200119422/Mendix/EventManagement/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
